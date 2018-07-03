8da15f50fda21bd62df72dfaa127f23f8640f20a
Yuri Pimenov <pimenoff@gmail.com>
2018-07-03 16:12:29 +0300

    Get rid of cacheing issues

--- DanakiDoctrineEnumTypeBundle.php
+++ DanakiDoctrineEnumTypeBundle.php
@@ -2,8 +2,13 @@
 
 namespace Danaki\DoctrineEnumTypeBundle;
 
+use Acelaya\Doctrine\Type\PhpEnumType;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DanakiDoctrineEnumTypeBundle extends Bundle
 {
+    public function boot()
+    {
+        PhpEnumType::registerEnumTypes($this->container->getParameter('danaki_doctrine_enum_type.types'));
+    }
 }
--- DependencyInjection/DanakiDoctrineEnumTypeExtension.php
+++ DependencyInjection/DanakiDoctrineEnumTypeExtension.php
@@ -4,12 +4,11 @@ namespace Danaki\DoctrineEnumTypeBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
-use Acelaya\Doctrine\Type\PhpEnumType;
 
 class DanakiDoctrineEnumTypeExtension extends ConfigurableExtension
 {
     protected function loadInternal(array $config, ContainerBuilder $container)
     {
-		PhpEnumType::registerEnumTypes($config['types']);
+        $container->setParameter('danaki_doctrine_enum_type.types', $config['types']);
     }
 }
