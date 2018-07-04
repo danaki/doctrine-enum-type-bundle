87dad5865fcdba8d142821fdb7ad1199831744a3
Yuri Pimenov <pimenoff@gmail.com>
2018-07-04 15:32:34 +0300

    Fix double type register

--- DanakiDoctrineEnumTypeBundle.php
+++ DanakiDoctrineEnumTypeBundle.php
@@ -3,12 +3,19 @@
 namespace Danaki\DoctrineEnumTypeBundle;
 
 use Acelaya\Doctrine\Type\PhpEnumType;
+use Doctrine\DBAL\Types\Type;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DanakiDoctrineEnumTypeBundle extends Bundle
 {
     public function boot()
     {
-        PhpEnumType::registerEnumTypes($this->container->getParameter('danaki_doctrine_enum_type.types'));
+
+        foreach ($this->container->getParameter('danaki_doctrine_enum_type.types') as $typeName => $enumClass) {
+            $typeName = \is_string($typeName) ? $typeName : $enumClass;
+            if (! Type::hasType($typeName)) {
+                PhpEnumType::registerEnumType($typeName, $enumClass);
+            }
+        }
     }
 }
